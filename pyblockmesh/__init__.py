9abdbd86bf51d08a41a4c623ddc66e0ba0fe32fd
Vishakh Kumar <grokkingStuff@gmail.com>
2018-09-14 00:34:03 +0400

    Adds better examples

--- pyblockmesh/__init__.py
+++ pyblockmesh/__init__.py
@@ -1,2 +1,11 @@
 from pyblockmesh.vertex import Vertex
 from pyblockmesh.hexahedron import Hexahedron
+from pyblockmesh.edge import Edge
+from pyblockmesh.face import Face
+
+def buildBlockMesh():
+    v = Vertex.list_all()
+    b = Hexahedron.list_all()
+    e = Edge.list_all()
+    f = Face.list_all
+    return "\n".join([v,b,e,f])
