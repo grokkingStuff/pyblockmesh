ea09e98ce6f647f60ccf09434a8e006a2f2dd142
Vishakh Kumar <grokkingStuff@gmail.com>
2018-09-13 23:21:10 +0400

    Adds development dependencies and updates setup.py with better classifiers

--- setup.py
+++ setup.py
@@ -2,16 +2,37 @@ import setuptools
 from packagename.version import Version
 
 
-setuptools.setup(name='py-boilerplate',
-                 version=Version('1.0.0').number,
-                 description='Python Package Boilerplate',
+setuptools.setup(name='pyblockMesh',
+                 version=Version('0.0.1').number,
+                 description='Python wrapper around blockMeshDict creation',
                  long_description=open('README.md').read().strip(),
-                 author='Package Author',
-                 author_email='you@youremail.com',
+                 author='Vishakh Kumar',
+                 author_email='grokkingStuff@gmail.com',
                  url='http://path-to-my-packagename',
-                 py_modules=['packagename'],
-                 install_requires=[],
+                 py_modules=['pyblockmesh'],
+                 install_requires=['collections'],
                  license='MIT License',
                  zip_safe=False,
-                 keywords='boilerplate package',
-                 classifiers=['Packages', 'Boilerplate'])
+                 python_requires='>=3'
+                 keywords='blockmesh blockmeshdict openfoam pyfoam',
+                 classifiers=[
+                             # How mature is this project? Common values are
+                             #   3 - Alpha
+                             #   4 - Beta
+                             #   5 - Production/Stable
+                             'Development Status :: 3 - Alpha',
+
+                             # Indicate who your project is intended for
+                             'Intended Audience :: Developers',
+
+                             # Pick your license as you wish (should match "license" above)
+                             'License :: OSI Approved :: MIT License',
+
+                             # Specify the Python versions you support here. In particular, ensure
+                             # that you indicate whether you support Python 2, Python 3 or both.
+                             'Programming Language :: Python :: 3',
+                             'Programming Language :: Python :: 3.2',
+                             'Programming Language :: Python :: 3.2',
+                             'Programming Language :: Python :: 3.3',
+                             'Programming Language :: Python :: 3.4',
+                             ])
